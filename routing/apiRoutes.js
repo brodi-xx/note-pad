36af2891e8fe112d21768242233689dc071135e3
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-07-05 11:36:03 -0700

    added in api routes and made an attempt at a delete path

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,10 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fs = require('fs');
 
 // GET (return db.json)
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
 
 // POST (return new note)
+router.post("/notes", (req, res) => {
+    res.json(notes);
+});
 
-
-module.exports = router;
+module.exports = router; 
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -5,12 +5,20 @@ const path = require('path');
 
 // GET (return index.html)
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/../routes/public/index.html'));
+    res.sendFile(path.join(__dirname, '/../public/index.html'));
 });
 
 // GET (return notes.html)
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, '/../public/notes.html'));
+});
+
+// DELETE (delete a note)
+router.delete("/notes/:id", (req, res) => {
+    const noteId = req.params.id;
+
+    // Return a response indicating the note was deleted
+    res.send(`Note with ID ${noteId} deleted successfully`);
 });
 
 module.exports = router;
