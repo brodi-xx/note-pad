6d705b7e0b118a08e2ac30d81d5311479870b6fa
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-07-28 17:42:27 -0700

    added 404.html page and route

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
