ab653182234dc2cfda4f2ed8c5872c187e9c9085
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-07-29 14:41:32 -0700

    file restructure

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,12 +5,12 @@ const path = require('path');
 
 // GET (return index.html)
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/../index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // GET (return notes.html)
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, '/../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../notes.html'));
 });
 
 // DELETE (delete a note)
--- server.js
+++ server.js
@@ -17,17 +17,17 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.sendFile(path.join(__dirname, '../index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/notes.html'))
+  res.sendFile(path.join(__dirname, '../notes.html'))
 );
 
 // route to direct users to a 404 page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/404.html'))
+  res.sendFile(path.join(__dirname, '../404.html'))
 );
 
 app.listen(PORT, () =>
